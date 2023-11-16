8de25c92fcfbe02ee0e905a8b3d7cbe82b5d33ac
root <root@Mei>
2023-11-16 10:19:07 +0900

    Add Akatsukizukuyo translation

--- src/data/lyrics/akatsukizukuyo.js
+++ src/data/lyrics/akatsukizukuyo.js
@@ -4,126 +4,126 @@ const akatsuki = {
             lyrics: "When I looked up to the skies above a single cloud\ndrifted far away, abandoning all the rest",
             details: {
                 original: "見上げた空には一つの雲が群からはぐれて",
-                jpTranslation: ""
+                jpTranslation: "見上げた空には雲が一つ\n群れからはぐれて遠くへ流れてる"
             }
         },
         {
             lyrics: "It was wandering alone like I am now\ntrying to find a breath of life\nLingering in this abyss of death",
             details: {
                 original: "今のボクと同じように屍の中で吐息を探してる",
-                jpTranslation: ""
+                jpTranslation: "今のボクと同じように孤独に彷徨って\n死の淵に漂う息吹を探してる"
             }
         },
         {
             lyrics: "It was my own hands that stained you red\nand I wonder, lost in my regrets\nWill there ever come a day \nwhen all these sins of mine are washed away?",
             details: {
                 original: "キミを朱く染めたこの手は\nいつの日にか洗い流せる時が来るのですか？",
-                jpTranslation: ""
+                jpTranslation: "キミを朱く染めた僕の手\n後悔に耽りながら思う\nボクの罪がいつか\n洗い流せる時が来るのですか？"
             }
         },
         {
             lyrics: "Until the day when I can show to you the way our dream should end\nLight up the stars, their fire flickers in the moonlit wind",
             details: {
                 original: "夢の続き描くその日まで\n風に揺れた星灯して",
-                jpTranslation: ""
+                jpTranslation: "夢の終わりを見せるその日まで\n星を照らし 炎は月夜の風に揺らめき"
             }
         },
         {
             lyrics: "Night goes on, the constellations turn until we meet again \nYou’ll smile like our future will shine\nAnd stay in my arms for all time",
             details: {
                 original: "いつか出逢えたときはもう一度\n微笑みを添えて\n強く抱きしめて",
-                jpTranslation: ""
+                jpTranslation: "夜は更けて星座は巡り また逢う日まで \nボクたちの未来が輝くように キミは微笑むだろう\nボクの腕の中でずっと"
             }
         },
         {
             lyrics: "All alone I search the clouds that hide\nWishes we made upon the star-filled skies\nIn darkness that consumes and leaves nothing behind",
             details: {
                 original: "あの日の夜空にキミと掛けた願いごと\n雲の隙間に一人探して",
-                jpTranslation: ""
+                jpTranslation: "満天の星空に賭けた願いを\n隠す雲を独り探してる\n全てを飲み込む闇の中で"
             }
         },
         {
             lyrics: "Even now you are the sin still haunting me\nwith only dead for company\nThe thought of daylight frightens me",
             details: {
                 original: "今もボクは罪を抱いて屍に凭れ\n夜明けに怯えてる",
-                jpTranslation: ""
+                jpTranslation: "今もずっと キミはボクを悩ませる罪\n死人だけが道連れ\n夜明けを思うだけで怯える"
             }
         },
         {
             lyrics: "Until the day when I can finally keep the promise that we made\nCount the stars, their fire shimmers in the pouring rain",
             details: {
                 original: "交わした約束叶うその日まで\n雨に濡れた星数えて",
-                jpTranslation: ""
+                jpTranslation: "交わした約束叶える日まで\n星を数えて 土砂降りの中 炎が煌めく"
             }
         },
         {
             lyrics: "The night goes on, the constellations turn\nUntil we meet again\nI’ll hold you close to my heart\nSo nothing can keep us apart",
             details: {
                 original: "いつか出逢えた時はもう二度と\n離れないように\n強く抱きしめて",
-                jpTranslation: ""
+                jpTranslation: "夜は更け 星座はめぐり\nまた会える日まで \nキミを胸に抱きしめて\n僕たちを引き離すものがないように"
             }
         },
         {
             lyrics: "Once again the daybreak comes \nWith such brilliant rays of sun \nI flinch, my eyes burning with loneliness \nIt won't let me rest",
             details: {
                 original: "また繰り返す眩い夜明けに\n目を背けたくなるほど寂しいけど",
-                jpTranslation: ""
+                jpTranslation: "また夜が明ければ \n太陽が眩しすぎて \nボクはたじろぎ 目は孤独に熱くなる\nボクを休ませない"
             }
         },
         {
             lyrics: "The rain breathes a gentle sigh \nCome to wash away the tears I've cried \nAnd even though my face is stained \nI'm calling your name",
             details: {
                 original: "溜息の雨が降り始めたとしても\nこの涙隠してくれるから",
-                jpTranslation: ""
+                jpTranslation: "雨は優しいため息をつき \nボクの泣いた涙を洗い流す \n顔が汚れていても \nキミの名前を呼ぶよ"
             }
         },
         {
             lyrics: "Broken and bleeding\nReach out your hands to me",
             details: {
                 original: "傷だらけの手を伸ばして",
-                jpTranslation: ""
+                jpTranslation: "傷つき血を流しながら\n手を伸ばして"
             }
         },
         {
             lyrics: "Until the day when I can show to you the way our dream should end\nLight up the stars, their fire flickers in the moonlit wind",
             details: {
-                original: "n夢の続き描くその日まで\n風に揺れた星灯して",
-                jpTranslation: ""
+                original: "夢の続き描くその日まで\n風に揺れた星灯して",
+                jpTranslation: "夢の終わりを見せるその日まで\n星を照らし 炎は月夜の風に揺らめき"
             }
         },
         {
             lyrics: "The night goes on, the constellations turn until we meet again \nYou’ll smile like our future will shine",
             details: {
                 original: "いつか出逢えたときはもう一度\n微笑みを添えて",
-                jpTranslation: ""
+                jpTranslation: "夜は更けて星座は巡り また逢う日まで \nボクたちの未来が輝くように キミは微笑むだろう"
             }
         },
         {
             lyrics: "Until the day when I can finally keep the promise that we made\nCount the lights, the stars are shining still in pouring rain",
             details: {
                 original: "交わした約束叶うその日まで\n雨に濡れた星数えて",
-                jpTranslation: ""
+                jpTranslation: "交わした約束叶える日まで\n灯りを数えて 土砂降りの中 星が今も煌めく"
             }
         },
         {
             lyrics: "The night will end, the world keeps turning round and when we meet again \nI won't let you go",
             details: {
                 original: "いつか出逢えたときはもう二度と\n離さない",
-                jpTranslation: ""
+                jpTranslation: "夜は終わり 世界は回り続ける \nいつか出逢えたときはもう二度と\n離さない"
             }
         },
         {
             lyrics: "On the day when I can finally keep the promise that we made\nThe full moon will shine triumphantly through our last dawn",
             details: {
                 original: "ボクが約束を叶える日には\n最後の夜明けに月を飾ろう",
-                jpTranslation: ""
+                jpTranslation: "ボクが約束を叶える日には\n最後の夜明けに月を燦々と輝かせよう"
             }
         },
         {
             lyrics: "A new day comes, the world keeps turning round until we meet again \nThe sunrise will bring a new start\nAnd nothing will keep us apart\nAnd nothing will keep us apart",
             details: {
                 original: "だから出逢えた時はもう誰も\n離れないように\n離れないように\n強く抱きしめて",
-                jpTranslation: ""
+                jpTranslation: "新しい日が訪れ 世界は回り続け ボクたちがまた会う日まで\n朝日が昇れば 新たな始まり\n僕たちを引き離せるものは何もない\n僕たちを引き離せるものは何もない"
             }
         }],
 filename: "akatsukizukuyo.musicxml"
--- src/data/lyrics/setsugekka.js
+++ src/data/lyrics/setsugekka.js
@@ -4,98 +4,98 @@ const setsugekka = {
             lyrics: "I whisper to you at nightfall \nAs the crescent moon shines on high \nCome dance once more through the snowfall \nLive again, return to my side",
             details: {
                 original: "三日月を抱いた君に呟いた\n「粉雪と踊る君に逢いたい…」",
-                jpTranslation: "夕暮れに三日月が高く輝く時に君に囁く\nもう一度雪の中で踊り、生きて、僕のそばに戻ってきて\n"
+                jpTranslation: "夕暮れ時に君に呟いた\n三日月が高く輝くとき \n粉雪の中でもう一度君と踊りたい\n生き返って、僕の元に来てほしい"
             }
         },
         {
             lyrics: "One thought I hold within my mind \nFor my thoughts lie where you are \nThe blood stains the snow at my fingers \nTo scatter the darkness like stars",
             details: {
                 original: "たった一つだけの想いを乗せて\n紅く染まる雪を宙に散りばめた",
-                jpTranslation: "一つの想いを心に抱く\n君と共に僕の想いがあるから\n血が指先で雪を染める\n星のように闇に散らす"
+                jpTranslation: "たった一つだけの想いを心に抱く\n僕の想いが君とともにあるから\n指先の雪を染める血\n星のように闇にばら撒いた"
             }
         },
         {
             lyrics: "Let it turn to flowers that shine \nAnd cradle you tight\nJust one feeling that never will die\nIt grows, blooming wild",
             details: {
                 original: "君を抱きしめる花となれ\n募る想いめぐらせ咲き乱れ",
-                jpTranslation: "輝いて、君を抱きしめる花に変わるように\nたった一つの消えざる想いが茂り、咲き乱れる"
+                jpTranslation: "輝いて、君を抱きしめる花になれ\n決して枯れることのない気持ちが\n募り、咲き乱れる"
             }
         },
         {
             lyrics: "I loved you, I could never let you go \nYour name from my lips in the falling snow \nThese frozen words keep spilling from my throat \nTo the night sky",
             details: {
                 original: "心奪われるほど君を愛せたことを\n何度も…何度も…夜空に叫んだ",
-                jpTranslation: "愛した、君を離すことができない\n降る雪の中、不意に君の名前を\n凍りつく言葉が喉から零れ続ける\n夜空へと"
+                jpTranslation: "愛していた 手放せなかった\n粉雪の中 ふと君の名前を…\n凍えた言葉が喉からこぼれ続ける\n夜空へ向かって"
             }
         },
         {
             lyrics: "I sing till my voice bleeds in still air \nAnd my breath turns white in the cold \nTonight when moonlight doth shimmer\nI still dream of holding you close",
             details: {
                 original: "吐息紅く染めて詠い続けてた\n「今宵の夢にぞ君が姿を…」",
-                jpTranslation: "静けさの中、声帯が血を流すまで\n白い息で歌い続ける\n今宵、月明かりが揺らめく頃に\nまた君を抱きしめる夢を見る"
+                jpTranslation: "静寂の中 声が枯れるまで詠い続けてた\n寒さで息が白くなる\n月の光がきらめく 今宵も\n君を抱きしめる夢を見ている"
             }
         },
         {
             lyrics: "As time flows, the seasons keep on turning \nAnd the winter steals your warmth \nThe last of your teardrops still linger \nIn my hands they are changing \nto endless snow",
             details: {
                 original: "君の消えた季節がもうすぐ終わる\n最後の涙　重ね、粉雪に変えて見せよう",
-                jpTranslation: "時が流れ、季節は巡り続ける\nそして冬が君の温もりを奪う\n君の最後の涙がまだ消えない\n僕の手の中で果てしない雪に変わっていく"
+                jpTranslation: "時は流れ 季節はめぐり \n冬は君の温もりを奪う\n君の最後の涙が今も漂う\n僕の手の中に\n止まない雪に変わっていく"
             }
         },
         {
             lyrics: "Let them turn to snowflakes that fly \nthrough winter’s dark skies \nLet them dance round you as crystals of ice \nBright stars in the night",
             details: {
                 original: "君を抱きしめる雪となれ\n募る想い散りばめ舞い踊れ",
-                jpTranslation: "冬の暗い空に舞い上がる雪片に変わるように\n雪の結晶になって君に寄せて舞い踊るように\n夜の輝く星として"
+                jpTranslation: "冬の暗い空を舞う結晶となれ\n君に寄り添って舞い踊れ\n夜の輝く星として"
             }
         },
         {
             lyrics: "I can’t hide wounds too deep to ever close\nA pain never-ending, the tears overflow \nYour last smile, a memory in icy mist \nNow fading in the dark and cold",
             details: {
                 original: "傷を隠そうとするほど何故か涙が溢れて\n君の微笑みが今、霞んで見えないよ",
-                jpTranslation: "永遠に塞がらないほど深い傷を隠せない\n終わりのない痛み、涙が溢れる\n君の最後の微笑みが凍える霧に囲まれた記憶\n今、暗闇と寒さの中で消えかけている"
+                jpTranslation: "塞がらない傷は隠せない\n終わらない痛み 溢れる涙 \n君の最後の微笑み 凍える霧の中の記憶 \n今 暗闇と寒さの中で消えゆく"
             }
         },
         {
             lyrics: "If someday I can be your moon \nI'll wander the skies above \nTrailing my tears \nAs stars 'cross the heavens \nTill my body crumbles to dust",
             details: {
                 original: "君を照らし出す月になれるなら\n夜空に星　散りばめて\n君を捜そう",
-                jpTranslation: "いつか君の月になれるなら\n高い空を彷徨おう\n天を渡って、涙を星として残す\nこの体が粉々に崩れるまで"
+                jpTranslation: "いつか君の月になれるなら\n空を彷徨う\n涙を星として天に残しながら\nこの身が塵となるまで"
             }
         },
         {
             lyrics: "Ever searching for where you are…",
             details: {
                 original: "躰　朽ち果てても…",
-                jpTranslation: "ずっと君を探し続けよう…"
+                jpTranslation: "ずっと君を探し続けるよ..."
             }
         },
         {
             lyrics: "Let it turn to snowflakes that fly \nTowards the night sky \nLet the flowers paint your image bright \nEmbraced by the moonlight",
             details: {
                 original: "君を抱きしめる雪となれ\n夜空の君を彩る花になれ",
-                jpTranslation: "夜空へ舞い上がる雪片に変わるように\n花が君の姿を鮮やかに描き出すように\n月明かりに抱かれながら"
+                jpTranslation: "夜空に向かって\n舞い散る雪となれ\n花は君の姿を鮮やかに彩る\n月の光に抱かれながら"
             }
         },
         {
-            lyrics: "The moment that I try to hold you close \nYou melt through my fingers \nA flower of snow",
+            lyrics: "The moment that I try to hold you close\nYou melt through my fingers\nA flower of snow",
             details: {
                 original: "君に触れようとするほど掴んでは消える…“雪の華”",
-                jpTranslation: "君を抱きしめようとしては\n溶けて指の間から落ちる\n雪の華"
+                jpTranslation: "君を抱きしめようとする瞬間\n指をすり抜けて溶ける\n雪の華"
             }
         },
         {
-            lyrics: "Across endless distance \nI call your name \nTo the frozen silence",
+            lyrics: "Across endless distance\nI call your name\nTo the frozen silence",
             details: {
                 original: "何度も…何度も…夜空に\n叫び続けた",
-                jpTranslation: "果てしない距離を超えて\n君の名前を呼び続ける\n凍りついた沈黙の中"
+                jpTranslation: "果てしない距離を越えて\n僕は君の名前を呼ぶよ\n凍り付いた静寂に向かって"
             }
         },
         {
             lyrics: "Till my voice reaches to you among the stars…",
             details: {
                 original: "君に届きますように…",
-                jpTranslation: "僕の声が星の間にいる君に届くまで…"
+                jpTranslation: "星の中にいる君に声が届くまで..."
             }
         }
     ],
