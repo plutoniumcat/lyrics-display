0dfa5accce9766c3eaaa0111ad84d58a968a7552
root <root@Mei>
2023-11-25 16:49:01 +0900

    finish copying arrow translation

--- src/data/lyrics/arrow.js
+++ src/data/lyrics/arrow.js
@@ -4,161 +4,161 @@ const arrow = {
             lyrics: "Let the wind take flight\nThrough the wasteland it soars\nAs it bids earth goodbye\nDancing till they're gone\nThe ashes will scorch the sky",
             details: {
                 original: "風よ舞え\n乾いた大地に別れを告げるまで\n夜空を焦がせ",
-                jpTranslation: "風よ羽ばたけ\n地上に別れを告げて\n荒野を飛んでいく\n消えるまでに舞い踊る灰が空を焦がす"
+                jpTranslation: "風よ舞え\n地上に別れを告げて\n荒野をを翔けて行く\n消えるまで舞い踊る\n灰は空を焦がす"
             }
         },
         {
             lyrics: "Who knows what lies beyond?\nThe last arrow flies through the night\nCarrying hope to a future unknown\nAs long as I can fight",
             details: {
                 original: "闇の中へ\nまだ見ぬ未来に最後の矢を解き放とう\n倒れるまで",
-                jpTranslation: "この先に何があるかと誰も知らない\n最後の矢が夜を貫いて\n未知の未来へ希望を運んでいく\n戦える限り"
+                jpTranslation: "その先に何があるのさ\n未知なる未来への希望を乗せて\n最後の矢が夜を貫く\n戦える限り"
             }
         },
         {
             lyrics: "If we're going to turn to dust\nIf the same fate lies in wait for all of us\nI want to make the moment last\nStand alone till I can have the final laugh",
             details: {
                 original: "いつかは誰もが塵となり消えるのなら\nオレだけはこのまま最後まで嗤ってたい",
-                jpTranslation: "もし俺たちは塵になるのなら\nもしみんな同じ運命に逢うのなら\nその一瞬を永遠に残したい\n最後に笑えるように一人立ちたい"
+                jpTranslation: "誰もが塵と化すのなら\n同じ運命が待っているのなら\nその瞬間を永遠に残したい\n最後に笑えるまで独り立ち向かいたい"
             }
         },
         {
             lyrics: "I know it's hard to take\nYour hands begin to shake\nThis nightmare that we face has no escape",
             details: {
                 original: "わかってるさ\n震えるほど恐いことだらけさ",
-                jpTranslation: "辛いだと分かっている\nお前の手が震え始める\n逃げようのない悪夢だ"
+                jpTranslation: "辛いのは わかってるさ\nオマエの手が震え出す\n逃げ場のない悪夢"
             }
         },
         {
             lyrics: "I want you by my side\nFrom now until I die\nI'll go out with a smile",
             details: {
                 original: "オマエの傍で微笑みながら\n朽ち果てよう",
-                jpTranslation: "今も、死ぬまでに\nそばにいてほしい\n笑って逝こう"
+                jpTranslation: "そばにいてほしい\n死ぬまでずっと\n笑って逝きたい"
             }
         },
         {
             lyrics: "Let the wind take flight \nThrough the wasteland it soars\nAs it bids earth goodbye \nDancing till they're gone\nThe ashes will scorch the sky",
             details: {
                 original: "風よ舞え\n乾いた大地に別れを告げるまで\n夜空を焦がせ",
-                jpTranslation: "風よ羽ばたけ\n地上に別れを告げて\n荒野を飛んでいく\n消えるまでに舞い踊る灰が空を焦がす"
+                jpTranslation: "風よ舞え\n地上に別れを告げて\n荒野をを翔けて行く\n消えるまで舞い踊る\n灰は空を焦がす"
             }
         },
         {
             lyrics: "Who knows what lies beyond?\nThe last arrow flies through the night\nCarrying hope to a future unknown",
             details: {
                 original: "闇の中へ\nまだ見ぬ未来に最後の矢を解き放とう",
-                jpTranslation: "この先に何があるかと誰も知らない\n最後の矢が夜を貫いて\n未知の未来へ希望を運んでいく"
+                jpTranslation: "その先に何があるのさ\n未知なる未来への希望を乗せて\n最後の矢が夜を貫く"
             }
         },
         {
             lyrics: "Even if you fall\nI hope you stay beautiful",
             details: {
                 original: "倒れるまで\n美しくあれ",
-                jpTranslation: "たとえ倒れようとも \n美しいままでいてほしい"
+                jpTranslation: "例え倒れようとも\n美しくいてほしい"
             }
         },
         {
             lyrics: "Falling raindrops come to shatter silence\nThe hammering of questions on broken earth\nYou sacrifice life, but does it make sense\nTo wage your war until the world is burned?",
             details: {
                 original: "静寂を毀す雨がひび割れた大地に問う\n「果てるまで仇すことにどんな意味があるのか･･」と",
-                jpTranslation: "降る雨の雫が静寂を壊しに来て\nひび割れた大地に絶え間なく問いかけている\n「命を犠牲しても、世界が燃え尽きるまで\n争い続けることに意味があるのか」と"
+                jpTranslation: "静寂を打ち砕く雨が降りしきる\nひび割れた大地に叩きつけて問う\n命を犠牲しても、世界が焼き尽くされるまで\n戦うことに意味があるのかと"
             }
         },
         {
             lyrics: "I know it's hard to take\nThis consequence is fate\nNo matter how we cry it's set in stone",
             details: {
                 original: "わかってるさ\n悔しいけど変わらぬことばかりさ",
-                jpTranslation: "辛いだと分かっている\nこの結果は運命だ\nどんなに泣いても、石に刻まれている\n（＝変えられない）"
+                jpTranslation: "辛いのは わかってるさ\nこれは運命\nどんなに泣いても 変わらないのさ"
             }
         },
         {
             lyrics: "The depths of hell await\nSurrounded by the flames\nThis fire's our own",
             details: {
                 original: "地獄の果てに辿り着くまで\n焼き尽くせ",
-                jpTranslation: "地獄の底が待つ\n火に囲まれていても\nこの炎は自分のものだ"
+                jpTranslation: "地獄の果てに辿り着くのさ\n炎に囲まれていても\nこの灯りはオレたちのものだ"
             }
         },
         {
             lyrics: "Carried by the rain\nThe teardrops still running\nFrom your lifeless gaze \nStart to fade away\nAnd I can only watch",
             details: {
                 original: "雨に凭れ\nその流した涙の跡が消えるまで\nただ見つめてた",
-                jpTranslation: "雨に流されて\nお前の命のない眼差しから\n流れ落ちている涙が消え始め\nそれを見守ることしかできない"
+                jpTranslation: "雨に流され\n生気のないオマエのまなざしから\nこぼれ落ちる涙が消えるのを\n見つめることしかできない"
             }
         },
         {
             lyrics: "I pull you close to me\nJust clinging to fragments\nRemains of our dreams\nYour body still bleeds",
             details: {
                 original: "夢を抱いて\n朽ち果てたオマエの躯 引き寄せた",
-                jpTranslation: "お前を引き寄せ\nその欠片、夢の跡にただしがみ付いている\nその躯がまだ血を流している"
+                jpTranslation: "オマエのこと 引き寄せた\n断片にしがみついて\n夢の残骸\n血が流れ続ける体"
             }
         },
         {
             lyrics: "I pray now to heaven \nMay your soul rest forever",
             details: {
                 original: "祈りよ届け\n安らかに眠れ",
-                jpTranslation: "今、天に祈る\n永遠に休めらせてたまえと"
+                jpTranslation: "天に届け\n安らかに眠れ"
             }
         },
         {
             lyrics: "(We were born into this world\nAt different places and times\nBut we wish to leave this world\nOn the same day, side by side\nI'll see you again\nThis is not the end)",
             details: {
                 original: "我は生まれた日\n時は違えども\n我は同じ日に\n死ぬことを願おう\nまたきっと、きっと会えるさ",
-                jpTranslation: "この世界に生まれた時と場所は\n違っていても\n同じ日に共に世界を去ることを願う\nきっとまた逢える\n最後じゃないから"
+                jpTranslation: "(それぞれの場所と時代に\nオレたちは生まれた\nそれでも 同じ日に\n肩を並べて\n死ぬことを願う\nきっとまた会える\nこれで終わりじゃないから)"
             }
         },
         {
             lyrics: "The ending is so violent\nWith no time left for any parting words\nOnly this bloodstained silence",
             details: {
                 original: "こんな終わり方で別れの言葉さえも\n何も言えないまま",
-                jpTranslation: "この結末があまりにも激しくて\n別れの言葉は何も言う時間もない\nただ血に濡れた沈黙だけ"
+                jpTranslation: "こんなひどい終わり方で\n別れの言葉さえも言えないまま\n血に染まった沈黙だけが残る"
             }
         },
         {
             lyrics: "Still I hold my last hope\nThat someday I'll see you in another life\nBut this time will you promise me\nThat I can stay by your side?",
             details: {
                 original: "だからせめて生まれ変われたとしたら\nまたあの日のように巡り会いたい",
-                jpTranslation: "それでも最後の希望を抱いている\nいつか生まれ変わってまた会えると\nでも今度は約束してくれるかい \nお前の傍にいられると"
+                jpTranslation: "それでも最後の希望はある\n生まれ変わってまた会えるんだ\n今度こそ約束してほしい\nオマエの側にいさせてくれるか？"
             }
         },
         {
             lyrics: "Let the wind take flight\nTill my body fails \nburned to dust by early light \nTo the endless fight \nI'll offer up my life",
             details: {
                 original: "風よ舞え\n夜明けに灼かれ塵になる瞬間まで\n命を賭して",
-                jpTranslation: "風よ羽ばたけ\n体が夜明けに焼かれ\n塵になって崩れるまで\n終わらない戦いに\n命を捧げよう"
+                jpTranslation: "風よ舞え\n体が夜明けに焼かれ\n塵になる瞬間まで\n終わりなき戦いに\n命を捧げよう"
             }
         },
         {
             lyrics: "Breathing my last breath \nI hold you and fire the last arrow left \nInto the dawn sky \nFor this is my goodbye",
             details: {
                 original: "吐息重ね\nオマエ抱いて最後の矢を解き放とう\n別れ添えて",
-                jpTranslation: "最後の一息とともに\nお前を抱いて最後の矢を\n明ける空に放つ\nこれが俺の「さよなら」だから"
+                jpTranslation: "息を引き取るとき\n最後の矢を解き放とう\n暁の空に\n別れを添えて"
             }
         },
         {
-            lyrics: "The day won't come yet\nDark hours still lie ahead\nBefore the future we dreamed will rise \nBut now you can close your eyes",
+            lyrics: "The day won't come yet\nDark hours still lie ahead\nBefore the future we dreamed will rise\nBut now you can close your eyes",
             details: {
                 original: "今はまだ\n描いた未来が訪れはしないけど\n瞳を閉じて",
-                jpTranslation: "今はまだ新しい日は来ない\n夢見た未来が明けるまでに\nまだ暗い時間があるけど\n瞳を閉じて"
+                jpTranslation: "今はまだその日は来なくて\n描いた未来が明ける前に\n暗い時間は続くけど\n瞳を閉じて"
             }
         },
         {
             lyrics: "Still I won't forget \nHow we smiled when we first met \nWith such innocence \nThe memory still bright",
             details: {
                 original: "輝いてた･･･\n無邪気な微笑みを浮かべていたあの日･･･",
-                jpTranslation: "それでも、忘れない\n初めて会った時は\n無邪気に微笑んでいたことを\n思い出は鮮やかなまま"
+                jpTranslation: "決して忘れない\n初めて出会った日は\n無邪気に微笑んでいたこと\nまだ明るい記憶"
             }
         },
         {
             lyrics: "And so each of our dreams \nCan shine like the daylight\nBeyond this pitch-black sky",
             details: {
                 original: "あの日に見たオレたちの夢が叶うために",
-                jpTranslation: "そして、俺たちの夢ごとが\nこの真っ黒な空を越えて\n日差しのように輝けるために"
+                jpTranslation: "それぞれの夢が\n漆黒の空の向こうに\n日の光のように輝くために"
             }
         },
         {
             lyrics: "Our lives scatter beautifully!",
             details: {
                 original: "美しく散れ",
-                jpTranslation: "俺たちの命が美しく散っていく"
+                jpTranslation: "俺たちの命が美しく散る"
             }
         }
     ],
